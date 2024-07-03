adb321e856008919f04e5f0d8ea81779dcdd1d71
Yuriy_K <krentawork@gmail.com>
2024-07-03 19:34:58 +0300

    minor fix

--- main.py
+++ main.py
@@ -12,14 +12,14 @@ def test_scenario():
     click_('/html/body/div/div/div[2]/div[2]/section/div[2]/button') # click authorisation
     click_('/html/body/div/div/div[2]/div[2]/section/div[2]/div/div[2]/div/div/div[2]/div/div/div[2]/div/div[4]/button[1]')
     authorisation(test_login, test_password)
-    # time.sleep(2000)
-    driver.switch_to.window(driver.window_handles[0])
-    click_('/html/body/div/div/div[2]/div[2]/section/div[2]/div/div[2]/div/div/div[1]/button/svg')
+    driver.switch_to.window(driver.window_handles[0]) # Пока что, всё останавливается на этом этапе
+    click_('/html/body/div/div/div[2]/div[2]/section/div[2]/div/div[2]/div/div/div[1]/button/svg') # закрыть окно авторизации
     click_('/html/body/div/div/div[2]/div[4]/section/div/span[1]/div/div/div/span[3]/div/div/button[3]/svg') # раскрываем список chat
     click_('/html/body/div/div/div[2]/div[4]/section/div/span[1]/div/div/div/span[3]/div/div[2]/div/div[1]/div[1]/div[2]/button') # try it
-    write_in('/html/body/div/div/div[2]/div[4]/section/div/span[1]/div/div/div/span[3]/div/div[2]/div/div[1]/div[2]/div/table/tbody/tr[1]/td[2]/input', '1')
     write_in('/html/body/div/div/div[2]/div[4]/section/div/span[1]/div/div/div/span[3]/div/div[2]/div/div[1]/div['
-             '3]/div[2]/div/div/div/textarea', test_data)
+             '2]/div/table/tbody/tr[1]/td[2]/input', '1')
+    write_in('/html/body/div/div/div[2]/div[4]/section/div/span[1]/div/div/div/span[3]/div/div[2]/div/div[1]/div['
+             '3]/div[2]/div/div/div/textarea', 'тут надо вставить переменную с json')
     driver.implicitly_wait(10)
     # assert check_success(what_search, where_search) is True # Тут будет проверка, что тест прошёл успешно.
 
