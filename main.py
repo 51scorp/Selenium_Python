7b34b7de8cfcdeb2bd407ebd00e2ca0756ca9ad8
y_krenta <y_krenta@eureca.ru>
2024-07-26 16:32:34 +0300

    test_complete

--- main.py
+++ main.py
@@ -1,29 +1,31 @@
 import time
 import math
 from selenium import webdriver
-# from data import *
+
+from data import *
 
 driver = webdriver.Chrome()
 from moduls import *
 
 
-
-def test_scenario():
+def test_add_to_cart():
     connect(url)  # connect to the site
-    click_('/html/body/div/div/div[2]/div[2]/section/div[2]/button') # click authorisation
-    click_('/html/body/div/div/div[2]/div[2]/section/div[2]/div/div[2]/div/div/div[2]/div/div/div[2]/div/div[4]/button[1]')
-    authorisation(test_login, test_password)
-    driver.switch_to.window(driver.window_handles[0]) # Пока что, всё останавливается на этом этапе
-    click_('/html/body/div/div/div[2]/div[2]/section/div[2]/div/div[2]/div/div/div[1]/button/svg') # закрыть окно авторизации
-    click_('/html/body/div/div/div[2]/div[4]/section/div/span[1]/div/div/div/span[3]/div/div/button[3]/svg') # раскрываем список chat
-    click_('/html/body/div/div/div[2]/div[4]/section/div/span[1]/div/div/div/span[3]/div/div[2]/div/div[1]/div[1]/div[2]/button') # try it
-    write_in('/html/body/div/div/div[2]/div[4]/section/div/span[1]/div/div/div/span[3]/div/div[2]/div/div[1]/div['
-             '2]/div/table/tbody/tr[1]/td[2]/input', '1')
-    write_in('/html/body/div/div/div[2]/div[4]/section/div/span[1]/div/div/div/span[3]/div/div[2]/div/div[1]/div['
-             '3]/div[2]/div/div/div/textarea', 'тут надо вставить переменную с json')
-    driver.implicitly_wait(10)
-    # assert check_success(what_search, where_search) is True # Тут будет проверка, что тест прошёл успешно.
+    authorisation(test_login, test_password)  # authorisation to the site
+    clear_cart()  # empty the cart before testing
+    write_in('/html/body/div[1]/div/div[2]/div[2]/div/header/div['
+             '1]/div/div/div/div/div/div/form/div/div/div/div/div/input[1]', what_search)  # what search
+    click_('/html/body/div[1]/div/div[2]/div[2]/div/header/div[1'
+           ']/div/div/div/div/div/div/form/div[1]/button')  # click search
+    click_('/html/body/div[1]/div/div[3]/div/div/div[2]/div/div/div[5]/div/div/div/div/div/div/div/div['
+           '3]/div/div/div/div[1]/div/div/noindex/div/button[2]')  # sort cheaper
+    click_('/html/body/div[1]/div/div[3]/div/div/div[2]/div/div/div[5]/div/div/div/div/div/aside/div/div['
+           '3]/div/div/div/div/div[4]/div/fieldset/div/div/div[2]/div[1]/div/div/div[1]/label/span')  # select 8 GB RAM
+    driver.refresh()  # refresh page
+    time.sleep(2)  # wait download page
+    click_first_match('_1TRiI')  # add to cart first product found
+    click_('/html/body/div[1]/div/div[2]/div[2]/div/header/div[1]'
+           '/div/div/noindex[2]/nav/ul/li[4]/div/div/div/a')  # goCart
+    time.sleep(2)  # wait download page
+    driver.save_screenshot("./log_screenshots/add_to_cart.png")
 
-    driver.close() # close the browser
-
-
+    driver.close()
--- moduls.py
+++ moduls.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 
 def connect(url):
     driver.get(url)
+    driver.maximize_window()  # maximize the window
     driver.implicitly_wait(10)
 
 
@@ -12,27 +13,45 @@ def click_(press_the_button):
     driver.implicitly_wait(10)
 
 
+def click_first_match(class_name):
+    driver.find_element(By.CLASS_NAME, class_name).click()
+    driver.implicitly_wait(10)
+
+
 def write_in(where_change, send_keys):
     element = driver.find_element(By.XPATH, where_change)
     element.clear()
     driver.implicitly_wait(10)
     element.send_keys(send_keys)
-    driver.implicitly_wait(10)
 
 
-def check_success(what_search, where_search):
-    element = driver.find_element(By.XPATH, where_search)
-    info = element.text
-    return what_search in info
+def authorisation(login, password):
+    click_(
+        '/html/body/div[1]/div/div[2]/div[2]/div/header/div[1]/div/div/noindex[2]/nav/ul/li[5]/div/div/div/div/div/a')
+    write_in('/html/body/div/div/div[2]/div[2]/div/div/div[2]/div[3]/div/div/div/div/form/div/div[2]/div['
+             '2]/div/div/span/input', login)  # идентификация
+    click_(
+        '/html/body/div/div/div[2]/div[2]/div/div/div[2]/div[3]/div/div/div/div/form/div/div[3]/div[2]/button')  # Войти
+    write_in('/html/body/div/div/div[2]/div[2]/div/div/div[2]/div[3]/div/div/form/div/div[2]/div[2]/div[1]/span/input',
+             password)  # аутентификация
+    click_('/html/body/div/div/div[2]/div[2]/div/div/div[2]/div[3]/div/div/form/div/div[3]/div[1]/button')  # продолжить
+    driver.implicitly_wait(30)  # Ожидание авторизации
 
 
-def else_flutter():  # если сайт написан на флаттер, нужно запустить скрипт
-    driver.execute_script("document.querySelector('flt-glass-pane').shadowRoot.querySelector("
-                          "'flt-semantics-placeholder').click();")
-
-# def authorisation(login, password):
-#     driver.switch_to.window(driver.window_handles[-1])
-#     write_in('/html/body/div/div[2]/div/div/div[1]/div[1]/form/div[1]/input', login)  # Пользователь
-#     write_in('/html/body/div/div[2]/div/div/div[1]/div[1]/form/div[2]/input', password)  # Пароль
-#     click_('/html/body/div/div[2]/div/div/div[1]/div[1]/form/div[4]/input[2]')
-#     driver.implicitly_wait(10)
+def clear_cart():
+    driver.find_element(By.XPATH,
+                        '/html/body/div[1]/div/div[2]/div[2]/div/header/div[1]/div/div/noindex[2]/nav/ul/li['
+                        '4]/div/div/div/a').click()  # goCart
+    driver.implicitly_wait(1)
+    try:  # Очистка корзины
+        driver.find_element(By.XPATH,
+                            '/html/body/div[1]/div/div[3]/div/div/div/div/main/div[2]/div/div[2]/div['
+                            '1]/div/div/div/ul/li/div/div/div/div[1]/div/div/div/div[2]/div[2]').click()  # clear Cart
+        driver.implicitly_wait(1)
+        driver.find_element(By.XPATH,
+                            '/html/body/div[1]/div/div[3]/div/div/div/div/main/div[2]/div/div[2]/div[1]/div/div/div/ul/'
+                            'li/div/div/div/div[1]/div/div/div/div[2]/div[2]/div[2]/div[2]/div/div[2]/button[2]').click()  # confirm clear
+        driver.implicitly_wait(1)
+        driver.back()
+    except:
+        driver.back()
