2be4cac1ceb55b66af98c3fca31c5d57556d18ca
y_krenta <y_krenta@eureca.ru>
2024-08-22 19:21:19 +0300

    step 5

--- conftest.py
+++ conftest.py
@@ -1,6 +1,7 @@
 import pytest
 import math
 import time
+import re
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from data import *
@@ -21,18 +22,22 @@ def driver():
 @pytest.mark.parametrize('version', list_sites)
 def test_guest_should_see_login_link(driver, version):
     link = f'{version}'
+    step = re.search(r'(\d+)', link)
     driver.get(link)
-    driver.maximize_window()  # maximize the window
+    # driver.maximize_window()  # maximize the window
+    driver.save_screenshot(f"./screenshots/{step}.png")
     driver.implicitly_wait(10)
     driver.find_element(By.ID, "ember459").click()
     driver.find_element(By.ID, "id_login_email").send_keys(login)
     driver.find_element(By.ID, "id_login_password").send_keys(password)
     driver.find_element(By.XPATH, '//*[@id="login_form"]/button').click()
-    driver.implicitly_wait(10)
-    input_field = driver.find_element(By.ID, "ember509")
+    time.sleep(4)
+    input_field = driver.find_element(By.XPATH, '/html/body/main/div[1]/div[2]/div/div[2]/div['
+                                                '1]/div/article/div/div/div[2]/div/section/div/div[1]/div['
+                                                '2]/div/div/div/textarea')
     input_field.clear()
     answer = math.log(int(time.time()))
     input_field.send_keys(str(answer))
-    driver.find_element(By.XPATH, '//*[@id="ember501"]/div/section/div/div[1]/div[4]/button')
-    time.sleep(10)
+    driver.find_element(By.CLASS_NAME, 'submit-submission').click()
+    # time.sleep(2)
 
