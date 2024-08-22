1304ea47fbd6675c23709e7edbaa1c33769f5281
y_krenta <y_krenta@eureca.ru>
2024-08-22 18:01:37 +0300

    step = 5

--- conftest.py
+++ conftest.py
@@ -1,9 +1,12 @@
 import pytest
+import math
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from data import *
 
 
-@pytest.fixture(scope="function", autouse=True)
+@pytest.fixture(scope="function")
 def driver():
     print("\nstart browser for test..")
     options = webdriver.ChromeOptions()
@@ -13,3 +16,23 @@ def driver():
     yield driver
     print("\nquit browser..")
     driver.quit()
+
+
+@pytest.mark.parametrize('version', list_sites)
+def test_guest_should_see_login_link(driver, version):
+    link = f'{version}'
+    driver.get(link)
+    driver.maximize_window()  # maximize the window
+    driver.implicitly_wait(10)
+    driver.find_element(By.ID, "ember459").click()
+    driver.find_element(By.ID, "id_login_email").send_keys(login)
+    driver.find_element(By.ID, "id_login_password").send_keys(password)
+    driver.find_element(By.XPATH, '//*[@id="login_form"]/button').click()
+    driver.implicitly_wait(10)
+    input_field = driver.find_element(By.ID, "ember509")
+    input_field.clear()
+    answer = math.log(int(time.time()))
+    input_field.send_keys(str(answer))
+    driver.find_element(By.XPATH, '//*[@id="ember501"]/div/section/div/div[1]/div[4]/button')
+    time.sleep(10)
+
--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-url = 'https://market.yandex.ru'
-
-
-def test_add_to_cart(driver):
-    connect(url, driver)
-
-
-def connect(url, driver):
-    driver.get(url)
-    driver.maximize_window()  # maximize the window
-    driver.implicitly_wait(10)
+# import time
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# from data import *
+# import math
+#
+# answer = math.log(int(time.time()))
+#
+#
+# def test_add_to_cart(driver):
+#     driver.find_element(By.ID, "ember459").click()
+#     driver.find_element(By.ID, "id_login_email").send_keys(login)
+#     driver.find_element(By.ID, "id_login_password").send_keys(password)
+#     driver.find_element(By.XPATH, '//*[@id="login_form"]/button').click()
+#     time.sleep(15)
+#
+#
+# # def connect(url_for_test, driver):
+# #     driver.get(url_for_test)
+# #     driver.maximize_window()  # maximize the window
+# #     driver.implicitly_wait(10)
